b605a64a92f09892bfc7488cf7af44c01ba894fc
NikZar <niko.zarzani@nautes.com>
2014-08-28 09:35:16 +0200

    heroku

---
+++ dist/elements/nautes-i18n.js
@@ -0,0 +1,114 @@
+var locale = {
+	"language" : {
+		"en-US": "LANGUAGE",
+		"it-IT": "LINGUA"
+	},
+	"hello" : {
+		"en-US": "Hi Man!",
+		"it-IT": "Ciao Bello!",
+		"fr-FR": "Bonjour Monsieur!"
+	},
+	"environment" : {
+		"en-US": "Environment",
+		"it-IT": "Ambiente"
+	},
+	"objects" : {
+		"en-US": "Objects",
+		"it-IT": "Oggetti"
+	},
+	"reactions" : {
+		"en-US": "Reactions",
+		"it-IT": "Reazioni"
+	},
+	"info" : {
+		"en-US": "Info",
+		"it-IT": "Informazioni"
+	},
+	"settings" : {
+		"en-US": "Settings",
+		"it-IT": "Impostazioni"
+	},
+	"family" : {
+		"en-US": "Users",
+		"it-IT": "Utenti"
+	},
+	"messages" : {
+		"en-US": "Messages",
+		"it-IT": "Messaggi"
+	},
+	"alerts" : {
+		"en-US": "Alerts",
+		"it-IT": "Avvisi"
+	},
+	"cr" : {
+		"en-US": "CREDENTIALS REQUIRED",
+		"it-IT": "CREDENZIALI RICHIESTE"
+	},
+	"show actions" : {
+		"en-US": "show actions",
+		"it-IT": "mostra azioni"
+	},
+	"hide actions" : {
+		"en-US": "hide actions",
+		"it-IT": "nascondi azioni"
+	},
+	"show stats" : {
+		"en-US": "show stats",
+		"it-IT": "mostra statistiche"
+	},
+	"hide stats" : {
+		"en-US": "hide stats",
+		"it-IT": "nascondi statistiche"
+	}
+	
+}
+
+PolymerExpressions.prototype.translate = function(key){
+
+	if(key){
+
+		if(!localStorage["locale"]){
+
+			var defaultLanguage = navigator.language;
+
+			if(!defaultLanguage){
+				defaultLanguage = navigator.browserLanguage;
+			}
+
+			switch(defaultLanguage){
+				case undefined : 
+				case "en-us":
+				case "en":{
+					localStorage["locale"] = JSON.stringify("en-US");
+					break;
+				}
+				case "it-it":
+				case "it":{
+					localStorage["locale"] = JSON.stringify("it-IT");
+					break;
+				}
+				default: {
+					localStorage["locale"] = JSON.stringify(defaultLanguage);
+				}
+			}
+
+		}
+
+		var currentLocale = JSON.parse(localStorage["locale"]);
+
+		var lower_key = key.toLowerCase();
+
+		var translation = "";
+		if( (lower_key in locale) && (currentLocale in locale[lower_key]) ){
+			return locale[lower_key][currentLocale];
+		} else if( (lower_key in locale) && ("en-US" in locale[lower_key]) ) {
+			return locale[lower_key]["en-US"];
+		} else {
+			return key;
+		}	
+	} else {
+		return "";
+	}
+	
+
+}
