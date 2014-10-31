d5c0943572bbff70d3d658a2a2d2530287691d45
Matteo Mazzoni <matteo@bestmazzo.it>
2014-10-31 22:54:26 +0100

    New translation approach: using i18next and importing official Freedomotic strings

--- app/elements/nautes-i18n.js
+++ app/elements/nautes-i18n.js
@@ -1,120 +1,58 @@
-var locale = {
-	"language" : {
-		"en-US": "LANGUAGE",
-		"it-IT": "LINGUA"
-	},
-	"hello" : {
-		"en-US": "Hi Man!",
-		"it-IT": "Ciao Bello!",
-		"fr-FR": "Bonjour Monsieur!"
-	},
-	"environment" : {
-		"en-US": "Environment",
-		"it-IT": "Ambiente"
-	},
-	"objects" : {
-		"en-US": "Objects",
-		"it-IT": "Oggetti"
-	},
-	"reactions" : {
-		"en-US": "Reactions",
-		"it-IT": "Reazioni"
-	},
-	"info" : {
-		"en-US": "Info",
-		"it-IT": "Informazioni"
-	},
-	"settings" : {
-		"en-US": "Settings",
-		"it-IT": "Impostazioni"
-	},
-	"family" : {
-		"en-US": "Users",
-		"it-IT": "Utenti"
-	},
-	"messages" : {
-		"en-US": "Messages",
-		"it-IT": "Messaggi"
-	},
-	"alerts" : {
-		"en-US": "Alerts",
-		"it-IT": "Avvisi"
-	},
-	"cr" : {
-		"en-US": "CREDENTIALS REQUIRED",
-		"it-IT": "CREDENZIALI RICHIESTE"
-	},
-	"show actions" : {
-		"en-US": "show actions",
-		"it-IT": "mostra azioni"
-	},
-	"hide actions" : {
-		"en-US": "hide actions",
-		"it-IT": "nascondi azioni"
-	},
-	"show stats" : {
-		"en-US": "show stats",
-		"it-IT": "mostra statistiche"
-	},
-	"hide stats" : {
-		"en-US": "hide stats",
-		"it-IT": "nascondi statistiche"
-	},
-	"setupinfo" : {
-		"en-US": "Info",
-		"it-IT": "Informazioni"
-	},
-	"setupsettings" : {
-		"en-US": "Settings",
-		"it-IT": "Impostazioni"
-	},
-	"setupobjects" : {
-		"en-US": "Objects",
-		"it-IT": "Oggetti"
-	},
-	
-}
+i18n.init({ lng: "it-IT", fallbackLng: 'en', debug: true}, function(){
 
-PolymerExpressions.prototype.translate = function(key){
+	if(!localStorage["locale"]){
 
-	if(key){
-		i18n.init({ lng: "en" });
-		if(!localStorage["locale"]){
+		var defaultLanguage = navigator.language;
 
-			var defaultLanguage = navigator.language;
-
-			if(!defaultLanguage){
-				defaultLanguage = navigator.browserLanguage;
-			}
-
-			switch(defaultLanguage){
-				case undefined : 
-				case "en-us":
-				case "en":{
-					localStorage["locale"] = JSON.stringify("en-US");
-					break;
-				}
-				case "it-it":
-				case "it":{
-					localStorage["locale"] = JSON.stringify("it-IT");
-					break;
-				}
-				default: {
-					localStorage["locale"] = JSON.stringify(defaultLanguage);
-				}
-			}
-
+		if(!defaultLanguage){
+			defaultLanguage = navigator.browserLanguage;
 		}
 
-		var currentLocale = JSON.parse(localStorage["locale"]);
+		switch(defaultLanguage){
+			case undefined : 
+			case "en-us":
+			case "en":{
+				localStorage["locale"] = JSON.stringify("en-US");
+				break;
+			}
+			case "it-it":
+			case "it":{
+				localStorage["locale"] = JSON.stringify("it-IT");
+				break;
+			}
+			default: {
+				localStorage["locale"] = JSON.stringify(defaultLanguage);
+			}
+		}
 
-		var lower_key = key.toLowerCase();
-console.log(i18n.t("app.name") + ": " + key);
-		return i18n.t(key);
-
-	} else {
-		return "";
 	}
-	
 
-}
+	PolymerExpressions.prototype.translate = function(key, extra){
+
+		if(key){
+
+			var currentLocale = JSON.parse(localStorage["locale"]);
+
+			var lower_key = key.toLowerCase();
+			//console.log(i18n.t("app.name") + ": " + key);
+			var translation;
+			if (!!extra){
+				translation = i18n.t(key, {"var": extra});
+			} else {
+				translation = i18n.t(lower_key);
+			}
+			if (!!translation) {
+				return translation;
+			} else {
+				return " _|" + lower_key + "|_ ";
+			}
+		} else {
+			return "[O_O]";
+		}
+	};
+
+	PolymerExpressions.prototype.upperCase = function(text){
+	 return text.toUpperCase();
+	};
+
+});
